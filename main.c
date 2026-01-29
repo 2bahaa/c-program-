50ca7dab7272018428954d2b8e6ebf4eaff2d790
Bahaa <mohamed2007886@miuegypt.edu.eg>
2026-01-29 11:50:37 +0200

    main push

--- main.c
+++ main.c
@@ -270,6 +270,7 @@ int num_patients = sizeof(patients) / sizeof(patients[0]);
     // Free dynamically allocated memory for patients
     //free(patients);
     //adding features
+    //main push
     return 0;
 }
 
